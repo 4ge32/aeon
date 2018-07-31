89c31d4a77de77d215b3f0f85f4a919807e68972
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2018-07-31 10:01:02 +0900

    Fix checking structure size when compiling code

--- super.c
+++ super.c
@@ -379,8 +379,9 @@ static int aeon_fill_super(struct super_block *sb, void *data, int silent)
 	int i;
 
 
-	BUILD_BUG_ON(sizeof(struct aeon_super_block) > AEON_SB_SIZE);
-	BUILD_BUG_ON(sizeof(struct aeon_inode) > AEON_SB_SIZE);
+	BUILD_BUG_ON(sizeof(struct aeon_super_block) == AEON_SB_SIZE);
+	BUILD_BUG_ON(sizeof(struct aeon_inode) == AEON_INODE_SIZE);
+	BUILD_BUG_ON(sizeof(struct aeon_dentry) == AEON_DENTRY_SIZE);
 
 	sbi = kzalloc(sizeof(struct aeon_sb_info), GFP_KERNEL);
 	if (!sbi)
