648e11b2e91cb72845089c9dd499752154824d28
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2019-02-07 16:08:51 +0900

    Disable to modify existing extents

--- extents.c
+++ extents.c
@@ -447,33 +447,33 @@ aeon_update_extent(struct super_block *sb, struct inode *inode,
 	struct aeon_inode *pi = aeon_get_inode(sb, sih);
 	struct aeon_extent_header *aeh = aeon_get_extent_header(pi);
 	struct aeon_extent *ae;
-	unsigned long next;
+	//unsigned long next;
 	int err = -ENOSPC;
 
 	/* TODO: separate this processing */
-	if (le16_to_cpu(aeh->eh_entries)) {
-		ae = aeon_get_prev_extent(aeh);
-		if (!ae)
-			goto new_alloc;
-
-		if (le16_to_cpu(ae->ex_length) > SHRT_MAX)
-			goto new_alloc;
-
-		next = le64_to_cpu(ae->ex_block) + le16_to_cpu(ae->ex_length);
-		if (next != blocknr)
-			goto new_alloc;
-
-		write_lock(&sih->i_meta_lock);
-
-		ae->ex_length += cpu_to_le16(num_blocks);
-		aeh->eh_blocks += cpu_to_le16(num_blocks);
-
-		write_unlock(&sih->i_meta_lock);
-
-		return 0;
-	}
-
-new_alloc:
+//	if (le16_to_cpu(aeh->eh_entries)) {
+//		ae = aeon_get_prev_extent(aeh);
+//		if (!ae)
+//			goto new_alloc;
+//
+//		if (le16_to_cpu(ae->ex_length) >= SHRT_MAX)
+//			goto new_alloc;
+//
+//		next = le64_to_cpu(ae->ex_block) + le16_to_cpu(ae->ex_length);
+//		if (next != blocknr)
+//			goto new_alloc;
+//
+//		write_lock(&sih->i_meta_lock);
+//
+//		ae->ex_length += cpu_to_le16(num_blocks);
+//		aeh->eh_blocks += cpu_to_le16(num_blocks);
+//
+//		write_unlock(&sih->i_meta_lock);
+//
+//		return 0;
+//	}
+//
+//new_alloc:
 	ae = aeon_get_new_extent(sb, sih);
 	if (!ae) {
 		aeon_err(sb, "can't expand file more\n");
